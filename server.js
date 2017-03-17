725d29b30ce24340833098baff687e5ef6546a55
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-17 19:11:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,8 @@ app.get('/', function (req, res) {
 
 
 app.get('/:articleName', function(req, res) {
-    res.send(createTemplate(articles[articleName]))
+    articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 } )
 
 // app.get('/article-one', function (req, res) {
