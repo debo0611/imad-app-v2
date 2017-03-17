86d7c14c27551d06efcf7104df7eb7731b124c1a
Debojyoti <debojyoti.pramanick@gmail.com>
2017-03-17 14:51:24 +0530

    revert/fix incorrect commit on server.js

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-<html>
-    <head>
-        <title>
-            Article One | Debojyoti IMAD
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-    </head>
-    <body>
-        <div>
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        
-        <h3>
-            Article One
-        </h3>
-        
-        <div>
-            Mar 14, 2017
-        </div>
-        
-        <div>
-            <p>
-                This is the content of my first Article
-            </p>
-            
-            <p>
-                This is the content of my first Article second paragraph
-            </p>
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-            <p>
-                This is the content of my first Article third paragraph
-            </p>
+var app = express();
+app.use(morgan('combined'));
 
-        </div>
-        </div>
-    </body>
-</html>
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res) {
+    // res.send('Article one requested and will be served here!');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+})
+
+
+app.get('/article-two', function (req, res) {
+    // res.send('Article two requested and will be served here!');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+})
+
+
+app.get('/article-three', function (req, res) {
+    // res.send('Article three requested and will be served here!');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+})
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
