4a91f4f8b35ba3758c20eec8fde5b93400ed8fc2
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-19 02:07:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,9 +117,11 @@ app.get('/counter', function(req, res) {
 
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+// app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxx
     // get the name from the request object
-    var name = req.params.name;
+    // var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 })
