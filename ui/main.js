0358a3963002eeda64fbd94f93470b6ff45743e5
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-18 10:11:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var button = document.getElementById("counter");
+button.click = function() {
+    // Make a request to the 'counter' endpoint
+    
+    // Capture the response and store it in a variable !
+    
+    // Render the variable in the correct span!
+}
