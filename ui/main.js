e10bf2d670425f0baad3be53e7f13072dfdfb997
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-18 10:48:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,27 @@ console.log('Loaded!');
 
 var button = document.getElementById("counter");
 button.click = function() {
-    // Make a request to the 'counter' endpoint
+    
+    // Create a request to the 'counter' endpoint
+    var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable !
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action ...
+            if (request.status === 200) {
+                var counter = rquest.responseText;
+                var span = document.getElementById("count");
+                span.InnerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
     
-    // Render the variable in the correct span!
-}
+    // Make the request ...
+    request.open('GET', 'http://debo0611.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+    
+
+};
