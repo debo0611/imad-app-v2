295e698d669dda18221537bff1ed7ccaab2c6d09
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-19 09:24:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,6 +49,7 @@ submit.onclick = function() {
                 // var names = ['name1', 'name2', 'name3'];
                 var names = request.responseText;
                 names = JSON.parse(names);
+                console.log("response: ", names);
                 var list = "";
                 for (var i = 0; i < names.length; i++) {
                     list += '<li>' + names[i] + '</li>';
