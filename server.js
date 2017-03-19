beacdd02cb6bedfbfb406d4e2289ae2f449fe255
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-19 15:25:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var http = require('http');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'debo0611',
+    database: 'debo0611',
+    host: 'db.hasura.app.io',
+    port: 5432,
+    password: process.env.DB_PASSWORD,
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -65,7 +74,6 @@ var articles = {
 }
 
 
-
 function createTemplate(data) {
     var title = data.title;
     var date = data.date;
@@ -125,7 +133,24 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxx
     console.log(req.query);
     names.push(name);
     res.send(JSON.stringify(names));
-})
+});
+
+
+// create the pool somewhere globally so its lifetime
+// lasts for as long as your app is running
+var pool = new Pool(config);
+app.get('/test-db', function() {
+   // make a request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   });
+   
+});
 
 
 app.get('/:articleName', function(req, res) {
