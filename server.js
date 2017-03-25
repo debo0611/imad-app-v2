1e74a9c39019b17849dcae480772e77cb20a4b83
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-25 20:28:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var config = {
     database: 'debo0611',
     host: 'db.imad.hasura-app.io', // host where db is hosted
     port: '5432',
-    password: process.env.DB_PASSWORD // use the env var available as DB_PASSWORD
-    // password: 'db-debo0611-44217'   
+    // password: process.env.DB_PASSWORD // use the env var available as DB_PASSWORD
+    password: 'db-debo0611-44217'   
 };
 
 var app = express();
