7d27ef5b8f2e2f55fba9c3000985f629f2370dbb
debo0611 <debojyoti.pramanick@gmail.com>
2017-03-25 20:41:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get('/articles/:articleName', function(req, res) {
             res.status(500).send(err.toString());
         } else {
             if (result.rows.length === 0) {
-                res.status(404).send(str(req.params.articleName) + 'Article NOT found!');
+                res.status(404).send('Article NOT found!');
             } 
             else {
                 var articleData = result.rows[0];
